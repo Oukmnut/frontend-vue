98e7c24d7108b310849cdcf51e46abc403c3e37b
Oukmnut <you@example.com>
2025-06-03 08:27:35 +0700

    build

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // 👈 THIS LINE FIXES ASSET LOADING IN DIST
   plugins: [
     vue(),
     vueDevTools(),
